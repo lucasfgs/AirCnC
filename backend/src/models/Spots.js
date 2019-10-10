1a9dcbb5d47e21e40fa2d65cedefdf1abef183c0
unknown <blooodcf@gmail.com>
2019-10-10 06:39:29 -0300

    Static spot images

--- backend/src/models/Spots.js
+++ backend/src/models/Spots.js
@@ -1,14 +1,25 @@
 const { model, Schema } = require("mongoose");
 
-const SportSchema = Schema({
-  thumbnail: String,
-  company: String,
-  price: Number,
-  techs: [String],
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
+const SpotSchema = Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:5000/files/${this.thumbnail}`;
 });
 
-module.exports = model("Spot", SportSchema);
+module.exports = model("Spot", SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 mongoose
   .connect(process.env.DB_CONNECTION_STRING, {
@@ -15,6 +16,7 @@ const routes = require("./routes");
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(process.env.PORT);
