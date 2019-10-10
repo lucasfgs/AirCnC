975507db9b52c6c3e1b4cbb9d47d27ef26b473a2
unknown <blooodcf@gmail.com>
2019-10-10 06:06:00 -0300

    User registration/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import api from "./services/api";
+
+import "./App.css";
+
+import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    let session = await api.post("sessions", {
+      email
+    });
+
+    const { _id } = session.data;
+
+    localStorage.setItem("user", _id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="Aircnc Logo" />
+
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para progamadores e encontre{" "}
+          <strong>talentos</strong> para sua empresa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="email"
+            id="email"
+            onChange={e => setEmail(e.target.value)}
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
