ddde8b9478eb53f692500f0e0d0196e88e88ca21
unknown <blooodcf@gmail.com>
2019-10-14 07:49:59 -0300

    Expo init mobile project

---
+++ mobile/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  txt: {
+    color: "red"
+  }
+});
